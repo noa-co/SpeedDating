c7e574054f36cb205c6ee5ca8c81d2243fe86ecb
Daniel Rubinov <drubinov@amazon.com>
2024-01-30 13:16:37 +0200

    added lambda demo

--- speed-dating-proj/src/components/homepage/homepage.jsx
+++ speed-dating-proj/src/components/homepage/homepage.jsx
@@ -1,11 +1,41 @@
 // HomePage.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 import Header from "../header/header";
 import "./homepage.css"
 import start_dating from '../../assets/start-dating.png'
+import { get } from 'aws-amplify/api';
+
+const myAPI = "apiTest";
+const path = "/test/1";
+
+
+async function invokeLambda() {
+    try {
+        const restOperation = get({
+            apiName: myAPI,
+            path: path
+        });
+        const response = await restOperation.response;
+        console.log('GET call succeeded: ', response);
+    } catch (error) {
+        console.log('GET call failed: ', error);
+    }
+}
+
+
 
 const HomePage = () => {
+    const [clicked, setClicked] = useState(false);
+    const handleClick = () => {
+        setClicked(true);
+    }
+    useEffect(() => {
+        if(clicked) {
+            invokeLambda();
+        }
+    }, [clicked]);
+
     return (
         <div className="main-div">
             <Header/>
@@ -19,9 +49,12 @@ const HomePage = () => {
                 >
                     <img src={start_dating} className="start-dating-image"/>
                     <div className="start-dating-text">Start Dating</div>
-                    <div className="start-dating-text">Hi moon</div>
                 </Button>
+                <div>
+                    <button onClick={handleClick}>invoke lambda</button>
+                </div>
             </div>
+
         </div>
     );
 };
