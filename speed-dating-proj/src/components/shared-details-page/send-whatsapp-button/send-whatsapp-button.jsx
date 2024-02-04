2d1e30280edae27a4f71c508ef570fbf63a0e0a7
noa-co <noa1999@windowslive.com>
2024-02-04 19:09:37 +0200

    some stuff

--- speed-dating-proj/src/components/shared-details-page/send-whatsapp-button/send-whatsapp-button.jsx
+++ speed-dating-proj/src/components/shared-details-page/send-whatsapp-button/send-whatsapp-button.jsx
@@ -1,8 +1,7 @@
-import React, {useState} from "react";
+import React from "react";
 import './send-whatsapp-button.css'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import IconButton from "@mui/material/IconButton";
-import SendMessage from "./send-message";
 
 
 
@@ -12,7 +11,7 @@ const SendWhatsAppBtn = ({phone, handleClick}) => {
     return (
         <div className={ "whatsapp-main whatsapp-"+phone}>
             <div className="send-whatsapp-btn-main">
-                <IconButton color="success" size="large" onClick={()=> handleClick}>
+                <IconButton color="success" size="large" onClick={handleClick}>
                     <WhatsAppIcon />
                 </IconButton>
             </div>
