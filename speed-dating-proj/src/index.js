701a90186d5272f06daa325c81b3b8981b6676da
Daniel Rubinov <drubinov@amazon.com>
2024-02-24 14:02:28 +0200

    added login with google

--- speed-dating-proj/src/index.js
+++ speed-dating-proj/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './aws-exports';
+import amplifyconfig from './amplifyconfiguration.json';
 
 Amplify.configure(amplifyconfig);
 
